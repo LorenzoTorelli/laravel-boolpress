709f9e6f7b2169ae50e3bdcc95ba8288a82f8edb
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2022-02-24 18:29:05 +0100

    first commit

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+        <h2>Sono il Footer</h2>
+    </footer>
+</template>
+
+<script>
+export default {
+    name:"Footer",
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <router-link class="link" :to="{ name: 'home' }">Home</router-link>
+                </li>
+                 <li>
+                    <router-link class="link" :to="{ name: 'about' }">About</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        height: 80px;
+        width: 100%;
+        border-bottom: 1px solid black;
+
+        nav {
+            height: 100%;
+            ul {
+                display: flex;
+                height: 100%;
+                align-items: center;
+                li {
+                    list-style: none;
+                    margin-left: 20px;
+                }
+            }
+        }
+        
+    }
+</style>
